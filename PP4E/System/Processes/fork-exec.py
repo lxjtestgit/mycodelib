40e8fbb5e44d0ee6df03c498c8a6b3a4a88ace1e
lxjtestgit <36828628+lxjtestgit@users.noreply.github.com>
2018-02-28 20:52:07 +0800

    分支新的进程，子程序运行全新的程序。
    在该程序钟，我们输入q之前，程序会一直分支出新的进程，但子进程不再在同一个文件中调用函数，而是运行一个全新的程序。

---
+++ PP4E/System/Processes/fork-exec.py
@@ -0,0 +1,14 @@
+"运行程序，直到你输入q"
+
+import os
+
+parm = 0
+while True:
+    parm == 1
+    pid = os.fork()
+    if pid ==0:         #复制进程
+        os.execlp('python','python','child.py',str(parm))       #覆盖原来的程序
+        assert False,'error starting program'       #不应该返回
+    else:
+        print('child is',pid)
+        if input() == 'q': break
