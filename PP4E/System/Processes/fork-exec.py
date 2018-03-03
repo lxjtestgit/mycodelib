4387c96878f5e2b44fe0be54717fb84c6a13a8c4
lxjtestgit <76690315@qq.com>
2018-03-03 17:16:07 +0800

    修改parm自增的错误

--- PP4E/System/Processes/fork-exec.py
+++ PP4E/System/Processes/fork-exec.py
@@ -4,7 +4,7 @@ import os
 
 parm = 0
 while True:
-    parm == 1
+    parm += 1
     pid = os.fork()
     if pid ==0:         #复制进程
         os.execlp('python','python','child.py',str(parm))       #覆盖原来的程序
